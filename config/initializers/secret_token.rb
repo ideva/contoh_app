d7c9c828ac2ee91290c2c08d10c13420226086c1
Agung Purbayana <agung.purbayana@gmail.com>
2012-07-26 21:42:32 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContohApp::Application.config.secret_token = '29b9f7940974359715e632c368236960cf78cfda1debc3e9354df83112f38265e5ec936a48fbbf0e6757fda4b3eb396d75c8268d27cb461b5e0e766142df2637'
